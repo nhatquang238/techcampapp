d21ecabd081c060872d6b2996ea1cf3a690b5fbb
nhatquang238 <nhatquang238@me.com>
2014-01-17 18:41:19 +0800

    setting up project

---
+++ framer/common.js
@@ -0,0 +1,19 @@
+for (var layerGroupName in PSD) {
+	// change local variables to global to easily refer to name
+	window[layerGroupName] = PSD[layerGroupName];
+
+	// save original frame's position
+	PSD[layerGroupName].originalFrame = window[layerGroupName].frame;
+
+	// save transitioned state as an attribute
+	PSD[layerGroupName].finalFrame = window[layerGroupName].originalFrame;
+}
+
+// set default animation function
+View.prototype.animateDefault = function (props) {
+	this.animate({
+		time: 300,
+		curve: 'spring(400,30,200)',
+		properties: props
+	});
+}
